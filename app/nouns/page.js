fb6914bd442c50bff900f0957f81e4d7ef389b8a
navilo0314 <navilo314@gmail.com>
2026-02-21 22:29:17 +0800

    update noun page menu

--- app/nouns/page.js
+++ app/nouns/page.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import QuizCatalog from '@/components/Navigation/QuizCatalog';
 
-const page = () => {
-  return (
-    <div>nouns page</div>
-  )
+export default function NounsPage() {
+  const nounCategories = [
+    {
+      title: 'Days of the Week',
+      description: 'Learn Japanese days: Monday to Sunday',
+      href: '/nouns/daysOfWeek',
+      emoji: '📅',
+    },
+    {
+      title: 'Universal Counter',
+      description: 'Count objects from one to ten',
+      href: '/nouns/universalCounter',
+      emoji: '🔢',
+    },
+  ];
+
+  return <QuizCatalog categories={nounCategories} />;
 }
-
-export default page
